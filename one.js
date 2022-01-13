2d3a2d8a955caf73f08f78068ba585227b02aaf0
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2022-01-13 12:39:28 +0100

    Initial commit

---
+++ one.js
@@ -0,0 +1,8 @@
+'use strict';
+const oneFunc = () => {
+    var content = document.getElementById('content');
+    var h1 = document.createElement('h1');
+    h1.innerText = 'One!';
+}
+
+oneFunc();
